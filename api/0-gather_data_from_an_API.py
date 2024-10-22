a91e814f333e0c983041fe3d41e2de0249eb1fc3
Sharif2138 <s.kiviiri@alustudent.com>
2024-10-22 14:48:05 +0200

    gujhgjghhgjn

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-"""Script to use a REST API for a given employee ID, returns
+""" the Script to use a REST API for a given employee ID, returns
 information about his/her TODO list progress"""
 import requests
+import sys
 
-get_todos(employee_id):
-user_data = requests.get(f"{USERS_URL}/{employee_id}").json()
-todos = requests.get(TODO_URL).json()
-employee_name = user_data["name"]
-employee_todo = []
-completed_emp_todos = []
-completed_totods_title = []
 
-for todo in todos:
-    if todo["userId"] == employee_id:
-        employee_todo.append(todo)
-    if todo["completed"] == True and todo ["userId"] == employee_id:
-        completed_emp_todos.append(todo)
-for todo in completed_emp_todos:
-    if todo["userId"] == employee_id:
-        completed_totods_title.append(todo["title"])
-print(f"Employee {employee_name} is done with tasks({len(completed_totods_title)}):")
-for title in completed_totods_title:
-    print(title)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"UsageError: python3 {__file__} employee_id(int)")
+        sys.exit(1)
+
+    API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
+
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
+
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
+
+    employee_name = data[0]["user"]["name"]
+    total_tasks = len(data)
+    done_tasks = [task for task in data if task["completed"]]
+    total_done_tasks = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks"
+          f"({total_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task['title']}")
