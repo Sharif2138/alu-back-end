82bf3250adb17347aec723b8fdd34a1b16e95740
Sharif2138 <s.kiviiri@alustudent.com>
2024-10-23 20:38:06 +0200

    khnn

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
         # Display the progress
         print(f"Employee {employee_name} is done with tasks"
-              f"({total_done_tasks}/{total_tasks}):")
+            f"({total_done_tasks}/{total_tasks}):")
         for task in done_tasks:
             print(f"\t {task['title']}")
 
