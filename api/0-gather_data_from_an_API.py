3fb14f79d4ae9fbaddceae641504a41d8aa14917
Sharif2138 <s.kiviiri@alustudent.com>
2024-10-23 20:39:27 +0200

    yuiu

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,7 +4,6 @@ information about his/her TODO list progress"""
 import requests
 import sys
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"UsageError: python3 {__file__} employee_id(int)")
@@ -19,12 +18,14 @@ if __name__ == "__main__":
         if user_response.status_code != 200:
             print("RequestError: User not found")
             sys.exit(1)
-        
+
         user_data = user_response.json()
         employee_name = user_data["name"]
 
         # Get the TODO list for the employee
-        todos_response = requests.get(f"{API_URL}/todos", params={"userId": EMPLOYEE_ID})
+        todos_response = requests.get(
+            f"{API_URL}/todos", params={"userId": EMPLOYEE_ID}
+        )
         if todos_response.status_code != 200:
             print("RequestError: Could not retrieve tasks")
             sys.exit(1)
@@ -36,7 +37,7 @@ if __name__ == "__main__":
 
         # Display the progress
         print(f"Employee {employee_name} is done with tasks"
-            f"({total_done_tasks}/{total_tasks}):")
+              f"({total_done_tasks}/{total_tasks}):")
         for task in done_tasks:
             print(f"\t {task['title']}")
 
